afe017f8c50ad78f0192940085af44b4016fc235
Eduardo Ghioldi <106218017+EdGhioldi@users.noreply.github.com>
2023-12-21 20:23:05 -0300

    Update Peluqueriacanina.java

--- src/main/java/com/mycompany/peluqueriacanina/Peluqueriacanina.java
+++ src/main/java/com/mycompany/peluqueriacanina/Peluqueriacanina.java
@@ -1,8 +1,14 @@
 package com.mycompany.peluqueriacanina;
+import com.mycompany.peluqueriacanina.igu.*;
 
 public class Peluqueriacanina {
 
     public static void main(String[] args) {
+        
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
 
     }
 }
