740f2b09814b3348d6d3ac35a185ecd36a4a810e
Eduardo Ghioldi <106218017+EdGhioldi@users.noreply.github.com>
2023-12-22 19:21:37 -0300

    Update Controladora.java

--- src/main/java/com/mycompany/peluqueriacanina/logica/Controladora.java
+++ src/main/java/com/mycompany/peluqueriacanina/logica/Controladora.java
@@ -1,4 +1,8 @@
 package com.mycompany.peluqueriacanina.logica;
+
+import com.mycompany.peluqueriacanina.persistencia.ControladoraPersistencia;
+
 public class Controladora {
+    ControladoraPersistencia controlPersist = new ControladoraPersistencia();
     
 }
